b6e912073f5accf411d3c8b41c9ff5d189700f7e
EdsonJSF <EdsonJSF1990@gmail.com>
2023-11-09 12:34:29 -0400

    Feat: MultipleCustomHooks display api information

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -5,12 +5,26 @@ export const MultipleCustomHooks = () => {
     "https://rickandmortyapi.com/api/character/1"
   );
 
-  console.log(data, isLoading, hasError);
+  console.log(hasError);
+
+  const { name, origin, image } = data;
 
   return (
     <>
       <h1>MultipleCustomHooks</h1>
       <hr />
+
+      {isLoading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote d-flex justify-content-end align-items-end gap-3">
+          <img src={image} alt={name + "image"} width="100" />
+          <div className="">
+            <p>{name}</p>
+            <footer className="blockquote-footer">{origin.name}</footer>
+          </div>
+        </blockquote>
+      )}
     </>
   );
 };
