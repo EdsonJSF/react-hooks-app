5af6a3071b962a7fd172b4d4ac8772d6ddb53bdb
EdsonJSF <EdsonJSF1990@gmail.com>
2023-11-17 12:26:14 -0400

    Feat: Todo list layout

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -21,15 +21,40 @@ export const TodoApp = () => {
 
   return (
     <>
-      <h1>Todo App</h1>
+      <h1>
+        Todo App: (10) <small>Waiting: (2)</small>
+      </h1>
       <hr />
 
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-      </ul>
+      <div className="row">
+        <div className="col-12 col-md-7">
+          <ul className="list-group">
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              <span>Item 1</span>
+              <button className="btn btn-danger">Del</button>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              <span>Item 2</span>
+              <button className="btn btn-danger">Del</button>
+            </li>
+            <li className="list-group-item d-flex justify-content-between align-items-center">
+              <span>Item 3</span>
+              <button className="btn btn-danger">Del</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-12 col-md-5">
+          <h4>Add Todo</h4>
+          <hr />
+
+          <form>
+            <input type="text" placeholder="body" className="form-control" />
+            <button type="submit" className="btn btn-outline-primary mt-1">
+              Add todo
+            </button>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
