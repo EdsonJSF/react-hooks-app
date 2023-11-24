48fe53508ee77258dd14bce5eebce6f052b9d623
EdsonJSF <EdsonJSF1990@gmail.com>
2023-11-24 12:47:52 -0400

    Feat: useContext

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,11 @@
+import { useState } from "react";
 import { UserContext } from "./UserContext";
 
-const user = {
-  id: 123,
-  name: "Name",
-  email: "email",
-};
-
 export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState();
+
   return (
-    <UserContext.Provider value={user}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
