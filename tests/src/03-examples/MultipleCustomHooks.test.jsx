48787306c0d7904786240e34837da5753390a17f
Edson Sánchez <edsonjsf1990@gmail.com>
2024-04-29 15:41:29 -0400

    feat: test on MultipleCustomHooks with useFetch mock

--- tests/src/03-examples/MultipleCustomHooks.test.jsx
+++ tests/src/03-examples/MultipleCustomHooks.test.jsx
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { MultipleCustomHooks } from "../../../src/03-examples/MultipleCustomHooks";
+import { useFetch } from "../../../src/hooks/useFetch";
+
+jest.mock("../../../src/hooks/useFetch.js");
 
 describe("Test on MultipleCustomHooks", () => {
   test("should show the default component", () => {
+    useFetch.mockReturnValue({
+      data: null,
+      isLoading: true,
+      hasError: null,
+    });
+
     render(<MultipleCustomHooks />);
 
     expect(screen.getByText("Loading..."));
@@ -11,5 +20,25 @@ describe("Test on MultipleCustomHooks", () => {
     const nextBtn = screen.getByRole("button", { name: "Next" });
 
     expect(nextBtn.disabled).toBe(true);
+  });
+
+  test("should show a Quote", () => {
+    useFetch.mockReturnValue({
+      data: { name: "test", image: "urlimage", origin: { name: "origin" } },
+      isLoading: false,
+      hasError: null,
+    });
+
+    render(<MultipleCustomHooks />);
+
+    expect(screen.getByText("test")).toBeTruthy();
+    expect(screen.getByText("origin")).toBeTruthy();
+
+    const img = screen.getByRole("img");
+    expect(img.src).toContain("urlimage");
+
+    const nextBtn = screen.getByRole("button", { name: "Next" });
+
+    expect(nextBtn.disabled).toBeFalsy();
   });
 });
